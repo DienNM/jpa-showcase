53a9c66048c49fe7dcf5800eac45d4cca5eb2796
DienNM <nmdien61@gmail.com>
2015-08-29 09:21:01 +0700

    Add jpa-sample-02: Demo of mapping type

---
+++ jpa-sample-02/README.md
@@ -0,0 +1,11 @@
+# JPA Hibernate Sample 02
+
+This is showing Mapping Samples in JPA
+
+    - Demo Simple Mapping Type
+
+Some Samples:
+    
+    - JPASampleMapping1Test: Mapping simple type of: byte, int, short, long, boolean, char, float, double
+    - JPASampleMapping2Test: Mapping simple type of : Byte, Integer, Short, Long, Boolean, Character, Float, Double
+    - JPASampleMapping3Test: Mapping simple type of : byte[], Byte[], char[], Character[]
---
+++ jpa-sample-02/src/test/java/com/dee/jpa/hibernate/JPASampleMapping2Test.java
@@ -0,0 +1,43 @@
+package com.dee.jpa.hibernate;
+
+import javax.persistence.EntityManager;
+
+import junit.framework.TestCase;
+
+import org.junit.Assert;
+
+import com.dee.jpa.hibernate.model.TypeMapping2Model;
+
+/**
+ * @author dien.nguyen
+ **/
+
+public class JPASampleMapping2Test extends TestCase{
+    
+    private TypeMapping2Model createTypeMappingModel() {
+        TypeMapping2Model typeMappingModel = new TypeMapping2Model();
+        typeMappingModel.setByteValue("a".getBytes()[0]);
+        typeMappingModel.setCharValue("a".toCharArray()[0]);
+        typeMappingModel.setDoubleValue(150.105);
+        typeMappingModel.setFloatValue(Float.valueOf(30));
+        typeMappingModel.setIntValue(23);
+        typeMappingModel.setLongValue(30l);
+        typeMappingModel.setShortValue((short)8);
+        
+        return typeMappingModel;
+    }
+
+    public void testJPA_CRUD() {
+        TypeMapping2Model typeMappingModel = createTypeMappingModel();
+        Assert.assertNull(typeMappingModel.getId());
+        EntityManager em = EntityManagerUtil.getEntityManager();
+        
+        // Persist User
+        em.getTransaction().begin();
+        em.persist(typeMappingModel);
+        em.getTransaction().commit();
+        Assert.assertNotNull(typeMappingModel.getId());
+        
+        em.close();
+    }
+}
